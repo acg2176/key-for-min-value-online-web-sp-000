7120a5c3285711ae0ffef1db3884d3abbc4a4067
Anna Guerrero <acg2176@columbia.edu>
2020-03-26 23:24:39 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,17 +1,6 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-def key_for_min_value(name_hash)
-  key_so_far, min_so_far = name_hash.first
-  name_hash.each do |name, value|
-     #first value
-    if min_so_far > value
-      min_so_far = value
-      name
-    end
-
-  end
-end
 
 def key_for_min_value(name_hash)
   array_vals = []
