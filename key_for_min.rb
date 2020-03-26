4a7697a1215ef5d38e592389d18129f49e76d59f
Anna Guerrero <acg2176@columbia.edu>
2020-03-26 23:07:32 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,8 +2,13 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
-  name_hash.collect {|name, value| value }
- #array of values
+  min_so_far = hash.values[0]
+  name_hash.each do |name, value|
+     #first value
+    if min_so_far > value
+      min_so_far = value
+      return name
+    end
+  end
 
 end
