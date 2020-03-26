f7f35796812e6c6f585b4332a9a8c1c441daead3
Anna Guerrero <acg2176@columbia.edu>
2020-03-26 23:34:03 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,22 +3,16 @@
 
 
 def key_for_min_value(name_hash)
-  array_vals = []
+  min_so_far = nil
+  key_so_far = nil
   name_hash.each do |name, value|
-    array_vals << value
-  end
-
-  array_vals.each_with_index do |num, index|
-    if array_vals[index] > array_vals[index + 1]
-      temp = array_vals[index]
-      array_vals[index] = array_vals[index + 1]
-      array_vals[index + 1] = temp
+    if min_so_far == nil
+      min_so_far = value
+      key_so_far = name
+    elsif min_so_far > value
+      min_so_far = value
+      key_so_far = name
     end
   end
-
-  name_hash.each do |name, value|
-    if value == array_vals[0] #minimum value
-      return name
-    end
-  end
+  key_so_far
 end
